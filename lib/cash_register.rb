7971605c5c038b4df0301e5243fa1fb3b0a76489
Ammar Ali <funnyguy118@gmail.com>
2019-02-20 22:54:27 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,20 @@
-
+class CashRegister
+  
+  attr_accessor :total, :discount
+  
+  def initialize (discount = 0)
+    @total = 0
+    @discount = discount
+  end
+  
+  def add_item(title, price, quantity = 1)
+    self.total = price * quantity
+  end
+  
+  def apply_discount
+  
+    self.total * (100 - @discount/100)
+  
+  end 
+  
+end
